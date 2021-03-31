6cf8d261c6f3a09a98bbf9f38e89c739bd20cc19
João Pedro Remedi Mota <jpremedi@hotmail.com>
2021-03-31 20:08:43 -0300

    personalização do conversor

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,16 +1,4 @@
-let valorEmDolarTexto = prompt("Qual valor em dólar que você quer converter?")
-console.log( typeof valorEmDolarTexto);
-// string = texto
-
-let valorEmDolarNumero = parseFloat(valorEmDolarTexto);
-console.log(typeof valorEmDolarNumero);
-// float = número decimal -> 12.99
-// in = número inteiro -> 12
-
-alert(valorEmDolarNumero);
-
+let valorEmDolarTexto = parseFloat(prompt("Qual valor em dólar que você quer converter?"));
 let valorEmReal = valorEmDolarNumero * 5.77;
-
 let valorEmRealFixado = valorEmReal.toFixed(2);
-
-alert(valorEmRealFixado);
+alert(valorEmRealFixado)
